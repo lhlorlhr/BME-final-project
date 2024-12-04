a77fd53c3cb20b9263460203ca9ad29b4aea6e16
Selina Park <112509732+prksyng54@users.noreply.github.com>
2024-12-04 00:12:31 -0600

    Add files via upload

--- image_generation.py
+++ image_generation.py
@@ -6,7 +6,7 @@ from skimage.transform import resize
 
 # 7(a) Population generation
 # Define parameters for grid, signal, and background
-grid_size = 64  # 64x64 pixels
+grid_size = 32  # 64x64 pixels
 signal_center = (0, 0) # Center of the signal (0, 0)
 sigma_s = 1 / 4  # Signal radius (σ_s)
 A_s = 3  # Signal amplitude
@@ -16,55 +16,37 @@ a_n = 1  # Lump amplitude
 M = 32 * 16 # Total number of sensitivity functions
 
 
-# def generate_grid(grid_size):
-#     x = np.linspace(-1/2, 1/2, grid_size) # rect(r)
-#     y = np.linspace(-1/2, 1/2, grid_size)
-#     r_x, r_y = np.meshgrid(x, y)
-#     return r_x, r_y
-
-# X, Y = generate_grid(grid_size)
-
-def circ(r, center_coords, sigma_s):
-    r_x, r_y = r
-    c_x, c_y = center_coords
-    return (r_x - c_x) ** 2 + (r_y-c_y) ** 2 <= sigma_s ** 2
-
-def rect(x):
-    return np.abs(x) < 0.5
-
-def pixelated(r_x, r_y):
-    return np.logical_and(rect(r_x), rect(r_y))
-
-def generate_signal(grid_size, signal_center, sigma_s = 1/4, A_s = 3):
-    x = np.linspace(-1, 1, grid_size) # rect(r)
-    y = np.linspace(-1, 1, grid_size)
+def generate_grid(grid_size):
+    x = np.linspace(-1/2, 1/2, grid_size) # rect(r)
+    y = np.linspace(-1/2, 1/2, grid_size)
     r_x, r_y = np.meshgrid(x, y)
-    signal = circ(r=(r_x, r_y), center_coords=(signal_center[0], signal_center[1]), sigma_s=sigma_s)
-    pixel = pixelated(r_x, r_y)
-    signal = A_s * pixel * signal
-    # r = np.sqrt((r_x - signal_center[0])**2 + (r_y - signal_center[1])**2)
-    # signal = np.zeros_like(r)
-    # signal[r <= sigma_s] = A_s
+    return r_x, r_y
+
+
+X, Y = generate_grid(grid_size)
+
+
+def generate_signal(X, Y, signal_center, sigma_s, A_s):
+    r = np.sqrt((X - signal_center[0])**2 + (Y - signal_center[1])**2)
+    signal = np.zeros_like(r)
+    signal[r <= sigma_s] = A_s
     return signal
 
-def generate_background(grid_size, N=10, sigma_n=1/4, a_n=1, num=500):
+
+def generate_background(grid_size, X, Y):
     background = np.zeros((grid_size, grid_size))
-    x = np.linspace(-1, 1, grid_size)
-    y = np.linspace(-1, 1, grid_size)
-    r_x, r_y = np.meshgrid(x, y)
-    pixel = pixelated(r_x, r_y)
 
     for _ in range(N):
         lump_center_x = np.random.uniform(-0.5, 0.5)
         lump_center_y = np.random.uniform(-0.5, 0.5)
         coeff = a_n / (2 * np.pi * sigma_n ** 2)
-        lump = coeff * np.exp(-((r_x - lump_center_x)**2 + (r_y - lump_center_y)**2) / (2 * sigma_n**2))
+        lump = coeff * np.exp(-((X - lump_center_x)**2 + (Y - lump_center_y)**2) / (2 * sigma_n**2))
         background += lump
 
-    return pixel * background
+    return background
 
-signal = generate_signal(grid_size, signal_center)
-background = generate_background(grid_size)
+signal = generate_signal(X, Y, signal_center, sigma_s, A_s)
+background = generate_background(grid_size, X, Y)
 signal_present = signal + background # Signal-present
 signal_absent = background  # Signal-absent
 
@@ -88,45 +70,30 @@ def plot_generated_objects(signal, background, signal_present):
     fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
     # Plot signal
-    axs[0].imshow(signal, extent=[-1, 1, -1, 1], cmap="viridis", origin="lower")
+    axs[0].imshow(signal, extent=[-1, 1, -1, 1], cmap="hot", origin="lower")
     axs[0].set_title("Signal (fs)")
     axs[0].set_xlabel("x")
     axs[0].set_ylabel("y")
-    axs[0].axis('off')
 
     # Plot background
-    axs[1].imshow(background, extent=[-1, 1, -1, 1], cmap="viridis", origin="lower")
+    axs[1].imshow(background, extent=[-1, 1, -1, 1], cmap="hot", origin="lower")
     axs[1].set_title("Background (fb)")
     axs[1].set_xlabel("x")
-    axs[1].axis('off')
 
     # Plot combined object
-    axs[2].imshow(signal_present, extent=[-1, 1, -1, 1], cmap="viridis", origin="lower")
+    axs[2].imshow(signal_present, extent=[-1, 1, -1, 1], cmap="hot", origin="lower")
     axs[2].set_title("Object with Signal (f = fs + fb)")
     axs[2].set_xlabel("x")
-    axs[2].axis('off')
 
     plt.tight_layout()
     plt.show()
     plt.close()
 
-plot_generated_objects(signal, background, signal_present)
-
 
 # Part 7(b) Image generation
 # Initialize lists to store results for different pixel sizes
 # pixel_sizes = [64, 32, 8]  # Dimensions for discretization
-pixel_sizes = 32 # If focus on one size
-
-
-def downsample(signal_present, signal_absent, factor=2):
-    """Downsample the image by averaging over non-overlapping blocks."""
-    # Get the shape of the downsampled image
-    new_size = signal_present.shape[0] // factor
-    # Reshape and compute the mean
-    signal_present = signal_present.reshape(new_size, factor, new_size, factor).mean(axis=(1, 3))
-    signal_absent = signal_absent.reshape(new_size, factor, new_size, factor).mean(axis=(1, 3))
-    return signal_present, signal_absent
+pixel_sizes = [32] # If focus on one size
 
 
 def generate_system_matrix(M, pixel_sizes):
@@ -134,32 +101,30 @@ def generate_system_matrix(M, pixel_sizes):
     num_strips = 32  # Number of horizontal strips = Number of sensitivity functions
     strip_width = 1 / 16  # Width is fixed
     num_rotations = 16
-    # for size in pixel_sizes:
-    H = np.zeros((M, pixel_sizes**2))
-    x = np.linspace(-0.5, 0.5, pixel_sizes)
-    y = np.linspace(-0.5, 0.5, pixel_sizes)
-    # x = np.linspace(-1, 1, size)
-    # y = np.linspace(-1, 1, size)
-    xx, yy = np.meshgrid(x, y)
-    # fov_mask = (xx**2 + yy**2) <= 1
+    for size in pixel_sizes:
+        H = np.zeros((M, size**2))
+        x = np.linspace(-0.5, 0.5, size)
+        y = np.linspace(-0.5, 0.5, size)
+        xx, yy = np.meshgrid(x, y)
+        # fov_mask = (xx**2 + yy**2) <= 1
 
-    for r in range(num_rotations):
-        rotation_angle = r * (np.pi / num_rotations)
-        rotation_matrix = np.array([
-            [np.cos(rotation_angle), -np.sin(rotation_angle)],
-            [np.sin(rotation_angle), np.cos(rotation_angle)]
-        ])
-        rotated_coordinates = np.dot(rotation_matrix, np.vstack([xx.ravel(), yy.ravel()]))
-        r_y = rotated_coordinates[1, :].reshape(xx.shape)
-        sensitivity = np.zeros_like(r_y)
+        for r in range(num_rotations):
+            rotation_angle = r * (np.pi / num_rotations)
+            rotation_matrix = np.array([
+                [np.cos(rotation_angle), -np.sin(rotation_angle)],
+                [np.sin(rotation_angle), np.cos(rotation_angle)]
+            ])
+            rotated_coordinates = np.dot(rotation_matrix, np.vstack([xx.ravel(), yy.ravel()]))
+            r_y = rotated_coordinates[1, :].reshape(xx.shape)
+            sensitivity = np.zeros_like(r_y)
 
-        for m in range(num_strips):
-            y_min = -1 + m / 16
-            y_max = -1 + (m + 1) / 16
-            strip_index = ((y_min < r_y) & (r_y <= y_max))
-            sensitivity[strip_index] = 1
-            matrix_index = r * num_strips + m
-            H[matrix_index, :] = sensitivity.ravel()
+            for m in range(num_strips):
+                y_min = -1 + m / 16
+                y_max = -1 + (m + 1) / 16
+                strip_index = ((y_min < r_y) & (r_y <= y_max))
+                sensitivity[strip_index] = 1
+                matrix_index = r * num_strips + m
+                H[matrix_index, :] = sensitivity.ravel()
 
         system_matrix.append(H)
 
@@ -220,17 +185,6 @@ def generate_reconstruction(projection_data, H_MP):
 
     return reconstructed_images
 
-# Add Poisson noise at different levels
-def generate_noise(projection_data):
-    noisy_projections = {}
-
-    for level, scale in noise_levels.items():
-        scaled_projections = projection_data * (scale / np.sum(projection_data, axis=1, keepdims=True))
-        noisy_projections[level] = np.random.poisson(scaled_projections)
-
-    return noisy_projections
-
-
 # Visualize a sample reconstruction for each noise level
 def plot_reconstruction(num_realizations, reconstructed_images, original_image, selected_size):
     sample_index = np.random.randint(num_realizations)
@@ -242,103 +196,36 @@ def plot_reconstruction(num_realizations, reconstructed_images, original_image, 
     for i, (level, recon_images) in enumerate(reconstructed_images.items()):
         axs[i + 1].imshow(recon_images[sample_index].reshape(selected_size, selected_size), cmap='viridis')
         axs[i + 1].set_title(f'Reconstruction ({level} Noise)')
-        axs[i + 1].axis('off')
 
     plt.tight_layout()
     plt.show()
-    plt.close()
+
+
 
 
 
 num_realizations = 500
-selected_size = 32
-noise_levels = {
-    "Low": 50000,
-    "Medium": 25000,
-    "High": 10000,
-    "Very_High": 2500
-}
+selected_size = pixel_sizes[0] # Example pixel size
 # system_matrices = {}  # Store system matrices for each pixel size
 system_matrices = generate_system_matrix(M, pixel_sizes)
 H = system_matrices[0]
 _, H_MP = generate_pseudoinverse(selected_size, H) # Print singular values and vectors or not?
 
-# Downsampling a 64x64 image to 32x32
-downsampled_present, downsampled_absent = downsample(signal_present, signal_absent)  # Downsample by 2
-# downsampled_absent = downsample(signal_absent)
-print("Original shape:", signal_present.shape)
-print("Downsampled signal present:", downsampled_present.shape)
-print("Downsampled signal absent:", downsampled_absent.shape)
-
-# signal_present_objects, signal_absent_objects = generate_multiple_objects(signal_present, signal_absent, grid_size = selected_size, J = 500)
-signal_present_objects, signal_absent_objects = generate_multiple_objects(downsampled_present, downsampled_absent, grid_size = selected_size, J = 500)
+signal_present_objects, signal_absent_objects = generate_multiple_objects(signal_present, signal_absent, grid_size = selected_size, J = 500)
 signal_present_realizations = signal_present_objects.reshape(500, -1)
 signal_absent_realizations = signal_absent_objects.reshape(500, -1)
 
-projection_data_present = np.dot(H, signal_present_realizations.T).T  # g = Hf, (512, 32x32) * (500, 32x32)^T = (512, 500)
-projection_data_absent = np.dot(H, signal_absent_realizations.T).T # (512, 500)^T = (500, 512)
-# projection_data = [projection_data_present, projection_data_absent]
+projection_data_present = np.dot(H, signal_present_realizations.T).T  # g = Hf, (500, grid x grid)
+projection_data_absent = np.dot(H, signal_absent_realizations.T).T
+projection_data = [projection_data_present, projection_data_absent]
 
-# noisy_projection_present = generate_noise(projection_data_present)
-# noisy_projection_absent = generate_noise(projection_data_absent)
+reconstructed_present = generate_reconstruction(projection_data[0], H_MP)
+reconstructed_absent = generate_reconstruction(projection_data[1], H_MP)
 
-reconstructed_present = generate_reconstruction(projection_data_present, H_MP)
-reconstructed_absent = generate_reconstruction(projection_data_absent, H_MP)
+reconstructed_present_graph = plot_reconstruction(num_realizations, reconstructed_present, signal_present_objects, selected_size)
+reconstructed_absent_graph = plot_reconstruction(num_realizations, reconstructed_absent, signal_absent_objects, selected_size)
 
-reconstructed__present_graph = plot_reconstruction(num_realizations, reconstructed_present, signal_present_objects, selected_size)
-reconstructed__absent_graph = plot_reconstruction(num_realizations, reconstructed_absent, signal_absent_objects, selected_size)
-
-# print(reconstructed_absent.shape)
-
-
-# 7(c) for CNN training
-
-
-
-# def prepare_datasets(noise_level, noisy_projection_present, noisy_projection_absent):
-#     # Shuffle the datasets
-#     np.random.seed(42)  # For reproducibility
-#     indices = np.random.permutation(1000)
-#     # Split indices for training, validation, and testing
-#     train_idx = indices[:600]
-#     val_idx = indices[600:800]
-#     test_idx = indices[800:]
-#     for noise_level in noisy_projection_present.keys():
-#         print(f"Processing noise level: {noise_level}")  # Debugging print
-
-#         noisy_images = np.concatenate([noisy_projection_present[noise_level], noisy_projection_absent[noise_level]], axis=0)
-#         low_noise_images = np.concatenate([noisy_projection_present["Low"], noisy_projection_absent["Low"]], axis=0)
-#         print(f"Noisy images shape before reshaping: {noisy_images.shape}")
-#         print(f"Low noise images shape before reshaping: {low_noise_images.shape}")
-
-#         # Check for NaN and infinite values
-#         if np.isnan(noisy_images).any() or np.isnan(low_noise_images).any():
-#             print(f"NaN detected in dataset generation for {noise_level}!")
-#             noisy_images = np.nan_to_num(noisy_images, nan=0.0)
-#             low_noise_images = np.nan_to_num(low_noise_images, nan=0.0)
-#         if not np.isfinite(noisy_images).all() or not np.isfinite(low_noise_images).all():
-#             print(f"Infinite values detected in dataset generation for {noise_level}!")
-#             noisy_images = np.nan_to_num(noisy_images, posinf=0.0, neginf=0.0)
-#             low_noise_images = np.nan_to_num(low_noise_images, posinf=0.0, neginf=0.0)
-
-#         # Reshape and save datasets
-#         np.save(f'train_data_{noise_level}.npy', noisy_images[train_idx])
-#         np.save(f'val_data_{noise_level}.npy', noisy_images[val_idx])
-#         np.save(f'test_data_{noise_level}.npy', noisy_images[test_idx])
-
-#         np.save('train_labels.npy', low_noise_images[train_idx])
-#         np.save('val_labels.npy', low_noise_images[val_idx])
-#         np.save('test_labels.npy', low_noise_images[test_idx])
-
-
-# train_data = np.load(f'train_data_{noise_level}.npy')
-# val_data = np.load(f'val_data_{noise_level}.npy')
-# test_data = np.load(f'test_data_{noise_level}.npy')
-
-# train_labels = np.load('train_labels.npy')
-# val_labels = np.load('val_labels.npy')
-# test_labels = np.load('test_labels.npy')
-
+# print(reconstructed_present.shape)
 
 
 
--- train_cnn.py
+++ train_cnn.py
@@ -2,215 +2,117 @@ import os
 import numpy as np
 from keras.models import load_model
 from keras.callbacks import EarlyStopping
-from keras import optimizers
 from DL_denoiser import get_cnn
 from image_generation import *
-import tensorflow as tf
-# from image_generation import generate_reconstruction, signal_present_objects, signal_absent_objects, H, H_MP
 from sklearn.metrics import mean_squared_error
+import tensorflow as tf
 
+# Debugging function
+def debug_noisy_images(reconstructed_present, reconstructed_absent):
+    for noise_level in reconstructed_present.keys():
+        diff = np.mean(reconstructed_present[noise_level] - reconstructed_absent[noise_level])
+        print(f"Noise Level: {noise_level}, Present-Absent Mean Difference: {diff}")
 
 def prepare_datasets():
     """
     Generate and save datasets for training, validation, and testing.
     """
-    num_realizations = signal_present_objects.shape[0]
-    # train_size = 300
-    # val_size = 100
-    # test_size = 100
-
-    # Generate projection data
-    projection_data_present = np.dot(H, signal_present_objects.reshape(num_realizations, -1).T).T
-    projection_data_absent = np.dot(H, signal_absent_objects.reshape(num_realizations, -1).T).T
-
+    # Projection data generation
     projection_data_present = np.dot(H, signal_present_realizations.T).T
     projection_data_absent = np.dot(H, signal_absent_realizations.T).T
 
     # Reconstruct noisy images
     reconstructed_present = generate_reconstruction(projection_data_present, H_MP)
     reconstructed_absent = generate_reconstruction(projection_data_absent, H_MP)
-    # noisy_projection_present = generate_noise(projection_data_present)
-    # noisy_projection_absent = generate_noise(projection_data_absent)
-    # reconstructed_present = noisy_projection_present
-    # reconstructed_absent = noisy_projection_absent
 
     for noise_level in reconstructed_present.keys():
-        print(f"Processing noise level: {noise_level}")  # Debugging print
-
+        print(f"Processing noise level: {noise_level}")
         noisy_images = np.concatenate([reconstructed_present[noise_level], reconstructed_absent[noise_level]], axis=0)
         low_noise_images = np.concatenate([reconstructed_present["Low"], reconstructed_absent["Low"]], axis=0)
 
-        print(f"Noisy images shape before reshaping: {noisy_images.shape}") # (1000, 1024)
-        print(f"Low noise images shape before reshaping: {low_noise_images.shape}") # (1000, 1024)
+        # Debugging: Check array shapes
+        print(f"Noise Level: {noise_level}, Noisy Images Shape: {noisy_images.shape}")
+        print(f"Low Noise Images Shape (Labels): {low_noise_images.shape}")
 
-        # Check for NaN and infinite values
-        if np.isnan(noisy_images).any() or np.isnan(low_noise_images).any():
-            print(f"NaN detected in dataset generation for {noise_level}!")
-            noisy_images = np.nan_to_num(noisy_images, nan=0.0)
-            low_noise_images = np.nan_to_num(low_noise_images, nan=0.0)
-        if not np.isfinite(noisy_images).all() or not np.isfinite(low_noise_images).all():
-            print(f"Infinite values detected in dataset generation for {noise_level}!")
-            noisy_images = np.nan_to_num(noisy_images, posinf=0.0, neginf=0.0)
-            low_noise_images = np.nan_to_num(low_noise_images, posinf=0.0, neginf=0.0)
-
-        # Normalize the data
+        # Normalize and convert data types
         max_noisy = np.max(np.abs(noisy_images))
+        noisy_images = noisy_images / max_noisy if max_noisy != 0 else 1
         max_low_noise = np.max(np.abs(low_noise_images))
-
-        if max_noisy == 0:
-            max_noisy = 1
-        if max_low_noise == 0:
-            max_low_noise = 1
-
-        noisy_images = noisy_images / max_noisy
-        low_noise_images = low_noise_images / max_low_noise
-
-        # Convert data types to float32
-        noisy_images = noisy_images.astype(np.float32)
-        low_noise_images = low_noise_images.astype(np.float32)
+        low_noise_images = low_noise_images / max_low_noise if max_low_noise != 0 else 1
 
         # Shuffle and split data
         indices = np.random.permutation(noisy_images.shape[0])
         train_idx = indices[:300]
         val_idx = indices[300:400]
         test_idx = indices[400:500]
-        print(f'training indices are: {train_idx}')
-        print(f'length of training indices: {len(train_idx)}')
 
-        # Reshape and save datasets
+        # Create directories and save files
         directory = f'./{noise_level}'
         os.makedirs(directory, exist_ok=True)
-        np.save(f'{directory}/train_data.npy', noisy_images[train_idx].reshape(-1, 32, 32, 1)) # (1000, 1024) --> (N, 32, 32, 1)
+        np.save(f'{directory}/train_data.npy', noisy_images[train_idx].reshape(-1, 32, 32, 1))
         np.save(f'{directory}/val_data.npy', noisy_images[val_idx].reshape(-1, 32, 32, 1))
         np.save(f'{directory}/test_data.npy', noisy_images[test_idx].reshape(-1, 32, 32, 1))
-        # np.save(f'train_data_{noise_level}.npy', noisy_images[train_idx].reshape(-1, 32, 32, 1))
-        # np.save(f'val_data_{noise_level}.npy', noisy_images[val_idx].reshape(-1, 32, 32, 1))
-        # np.save(f'test_data_{noise_level}.npy', noisy_images[test_idx].reshape(-1, 32, 32, 1))
 
-        np.save('train_labels.npy', low_noise_images[train_idx].reshape(-1, 32, 32, 1)) # ground truth
-        np.save('val_labels.npy', low_noise_images[val_idx].reshape(-1, 32, 32, 1))
-        np.save('test_labels.npy', low_noise_images[test_idx].reshape(-1, 32, 32, 1))
+        # Save ground truth labels
+        np.save(f'{directory}/train_labels.npy', low_noise_images[train_idx].reshape(-1, 32, 32, 1))
+        np.save(f'{directory}/val_labels.npy', low_noise_images[val_idx].reshape(-1, 32, 32, 1))
+        np.save(f'{directory}/test_labels.npy', low_noise_images[test_idx].reshape(-1, 32, 32, 1))
+        print(f"Saved train_labels.npy for noise level {noise_level} in {directory}")
 
-# def loss_fn(y_true, y_pred):
-#     # mean square error
-#     data_fidelity = tf.reshape(y_true, shape=[-1]) - tf.reshape(y_pred, shape=[-1])
-#     data_fidelity = tf.reduce_mean(tf.square(data_fidelity))
-#     return data_fidelity
-
-# Load datasets
 def load_data(noise_level):
     """
     Load training, validation, and test datasets for a given noise level.
     """
-    # train_data = np.load(f'train_data_{noise_level}.npy')
-    # val_data = np.load(f'val_data_{noise_level}.npy')
-    # test_data = np.load(f'test_data_{noise_level}.npy')
     train_data = np.load(f'./{noise_level}/train_data.npy')
     val_data = np.load(f'./{noise_level}/val_data.npy')
     test_data = np.load(f'./{noise_level}/test_data.npy')
-
-    train_labels = np.load('train_labels.npy')
-    val_labels = np.load('val_labels.npy')
-    test_labels = np.load('test_labels.npy')
-
-    print(f'training data shape: {train_data.shape}')
-    print(f'validation data shape: {val_data.shape}')
-    print(f'test data shape: {test_data.shape}')
-
+    train_labels = np.load(f'./{noise_level}/train_labels.npy')
+    val_labels = np.load(f'./{noise_level}/val_labels.npy')
+    test_labels = np.load(f'./{noise_level}/test_labels.npy')
     return train_data, train_labels, val_data, val_labels, test_data, test_labels
 
-
-# Train CNN
-def train_cnn(noise_level, epochs=100, batch_size=16, img_dim=32): # epochs=50
+def train_cnn(noise_level, epochs=100, batch_size=16):
     """
     Train the CNN for a specific noise level.
     """
     train_data, train_labels, val_data, val_labels, _, _ = load_data(noise_level)
 
-    # Define model
-    model = get_cnn(img_dim)
-    # model.summary()
-    # optimizer = optimizers.Adam(learning_rate=1e-3, )
+    model = get_cnn(32)
     model.compile(optimizer='adam', loss='mean_squared_error')
 
-    # Train model
     early_stop = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
     history = model.fit(
         train_data, train_labels,
         validation_data=(val_data, val_labels),
         epochs=epochs,
         batch_size=batch_size,
-        shuffle = True,
+        shuffle=True,
         callbacks=[early_stop]
     )
-    print('Training finished.')
-    train_loss = history.history['loss'] # training loss
-    val_loss = history.history['val_loss'] # validation loss
-    np.save(f'./{noise_level}/train_loss_epochs{epochs}.npy',train_loss) # training loss saved in npy file. Use provided readNPY() MATLAB function to read.
-    np.save(f'./{noise_level}/val_loss_epochs{epochs}.npy',val_loss)# validation loss saved in npy file. Use provided readNPY() MATLAB function to read.
-
-    # Save model
-    # model.save(f'cnn_denoising_{noise_level}.h5')
     model.save(f'./{noise_level}/cnn_denoising_model.h5')
 
-    return history
-
-
-# Evaluate model
-def evaluate_model(noise_level, img_dim=32, batch_size=16, num_test=500):
+def evaluate_model(noise_level, batch_size=16):
     """
     Evaluate the trained CNN model.
     """
     _, _, _, _, test_data, test_labels = load_data(noise_level)
-
-    # Load trained model
-    # model = load_model(f'cnn_denoising_{noise_level}.h5')
     model = load_model(f'./{noise_level}/cnn_denoising_model.h5')
+    denoised_images = model.predict(test_data, batch_size=batch_size)
 
-    # Predict denoised images
-    denoised_images = model.predict(test_data,
-                                    batch_size=batch_size)
-    print(f'test_images has {test_data.shape}')
-    print(f'denoised_images has {denoised_images.shape}')
-
-    print('Saving the predicted data...')
-    for i in np.arange(100):
-        current_pred = denoised_images[i]
-        directory = f'./{noise_level}/prediction'
-        os.makedirs(directory, exist_ok=True)
-        np.save(f'{directory}/image{i}.npy',current_pred)
-        # To read them, use provided readNPY() MATLAB function;
-
-    # Check for NaN values
-    if np.isnan(test_labels).any():
-        print("NaN found in test_labels!")
-        test_labels = np.nan_to_num(test_labels, nan=0.0)
-
-    if np.isnan(denoised_images).any():
-        print("NaN found in denoised_images!")
-        denoised_images = np.nan_to_num(denoised_images, nan=0.0)
-
-    # Compute RMSE
     rmse = np.sqrt(mean_squared_error(test_labels.flatten(), denoised_images.flatten()))
     print(f"RMSE for {noise_level} noise level: {rmse}")
-
     return denoised_images, rmse
 
-
-# Main Execution
 if __name__ == "__main__":
-    print("Preparing datasets...")
     prepare_datasets()
-    metrics = {'rmse': {}}
+    metrics = {}
 
     noise_levels = ['Medium', 'High', 'Very_High']
     for noise in noise_levels:
-        print(f"Training CNN for {noise} noise level...")
         train_cnn(noise)
 
     for noise in noise_levels:
-        print(f"Evaluating CNN for {noise} noise level...")
-        denoised_images, metric = evaluate_model(noise)
-        metrics[noise] = metric
+        _, rmse = evaluate_model(noise)
+        metrics[noise] = rmse
 
     print(metrics)
